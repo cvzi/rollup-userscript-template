7bcacc2ad8cbb6eee7f0c012bec2ea6b3c59b87f
cvzi <cuzi@openmail.cc>
2020-11-02 14:10:22 +0100

    gpr

---
+++ .github/workflows/gpr-name.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const path = require('path')
+
+function changeName(fileName, name) {
+  const data = JSON.parse(fs.readFileSync(fileName, 'utf8'))
+  data.name = name
+  fs.writeFileSync(fileName, JSON.stringify(data, null, 2))
+}
+
+process.argv.slice(3).forEach(fileName => changeName(fileName, process.argv[2]))
