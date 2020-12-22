951f7e161e64aa44961a560c1af3dae254ebac34
cvzi <cuzi@openmail.cc>
2020-12-22 20:47:02 +0100

    v1.2.0

--- dist/bundle.user.js
+++ dist/bundle.user.js
@@ -5,7 +5,7 @@
 // @require     https://unpkg.com/react@17/umd/react.development.js
 // @require     https://unpkg.com/react-dom@17/umd/react-dom.development.js
 // @include     https://github.com/*
-// @version     1.1.6
+// @version     1.2.0
 // @homepage    https://github.com/cvzi/rollup-userscript-template
 // @author      cuzi
 // @license     MIT
