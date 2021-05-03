75a40c87d0d3a86069e2c7e5bae48519ccfd5ef8
cvzi <cuzi@openmail.cc>
2021-05-03 11:09:51 +0200

    update dependencies and build

--- dist/bundle.user.js
+++ dist/bundle.user.js
@@ -40,6 +40,29 @@ SOFTWARE.
 (function (ReactDOM, React$1) {
   'use strict';
 
+  function _interopNamespace(e) {
+    if (e && e.__esModule) return e;
+    var n = Object.create(null);
+    if (e) {
+      Object.keys(e).forEach(function (k) {
+        if (k !== 'default') {
+          var d = Object.getOwnPropertyDescriptor(e, k);
+          Object.defineProperty(n, k, d.get ? d : {
+            enumerable: true,
+            get: function () {
+              return e[k];
+            }
+          });
+        }
+      });
+    }
+    n['default'] = e;
+    return Object.freeze(n);
+  }
+
+  var ReactDOM__namespace = /*#__PURE__*/_interopNamespace(ReactDOM);
+  var React__namespace = /*#__PURE__*/_interopNamespace(React$1);
+
   var helloWorld = (function () {
     window.setTimeout(function delayedError() {
       throw 'I am a delayed error';
@@ -51,28 +74,28 @@ SOFTWARE.
       return () => GM.getValue('test', 'a default value');
   })();
 
-  class SomeList extends React$1.Component {
+  class SomeList extends React__namespace.Component {
       constructor(props) {
           super(props);
       }
       render() {
-          return (React$1.createElement("div", { className: "some-list" },
-              React$1.createElement("h1", null,
+          return (React__namespace.createElement("div", { className: "some-list" },
+              React__namespace.createElement("h1", null,
                   "This is a list for ",
                   this.props.name),
-              React$1.createElement("ul", null,
-                  React$1.createElement("li", null, "plain javascript"),
-                  React$1.createElement("li", null, "typescript"),
-                  React$1.createElement("li", null, "react"),
-                  React$1.createElement("li", null, "JSX/TSX"))));
+              React__namespace.createElement("ul", null,
+                  React__namespace.createElement("li", null, "plain javascript"),
+                  React__namespace.createElement("li", null, "typescript"),
+                  React__namespace.createElement("li", null, "react"),
+                  React__namespace.createElement("li", null, "JSX/TSX"))));
       }
   }
 
-  ReactDOM.render( /*#__PURE__*/React.createElement(SomeList, {
+  ReactDOM__namespace.render( /*#__PURE__*/React.createElement(SomeList, {
     name: helloWorld
   }), document.body);
   getSomeValueFromGM().then(function (s) {
-    ReactDOM.render( /*#__PURE__*/React.createElement(SomeList, {
+    ReactDOM__namespace.render( /*#__PURE__*/React.createElement(SomeList, {
       name: s
     }), document.body);
   });
